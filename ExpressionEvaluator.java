1ddc02632bbaeb946da09db266fa58cbe64051ae
DhatriJangam <dhatrijangam602@gmail.com>
2024-07-23 19:38:20 +0530

    Create ExpressionEvaluator.java

---
+++ ExpressionEvaluator.java
@@ -0,0 +1,90 @@
+public class ExpressionEvaluator {
+
+    public static int precedence(char ch) {
+        switch (ch) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+        }
+        return -1;
+    }
+
+    public static String infixToPostfix(String expression) {
+        StringBuilder result = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+            if (Character.isLetterOrDigit(c)) {
+                result.append(c);
+            }
+            
+            else if (c == '(') {
+                stack.push(c);
+            }
+        
+            else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    result.append(stack.pop());
+                }
+                stack.pop();
+            } else { 
+                while (!stack.isEmpty() && precedence(c) <= precedence(stack.peek())) {
+                    result.append(stack.pop());
+                }
+                stack.push(c);
+            }
+        }
+        while (!stack.isEmpty()) {
+            result.append(stack.pop());
+        }
+
+        return result.toString();
+    }
+
+    public static int evaluatePostfix(String expression) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+
+            if (Character.isDigit(c)) {
+                stack.push(c - '0');
+            } else {
+                int val1 = stack.pop();
+                int val2 = stack.pop();
+
+                switch (c) {
+                    case '+':
+                        stack.push(val2 + val1);
+                        break;
+                    case '-':
+                        stack.push(val2 - val1);
+                        break;
+                    case '*':
+                        stack.push(val2 * val1);
+                        break;
+                    case '/':
+                        stack.push(val2 / val1);
+                        break;
+                }
+            }
+        }
+        return stack.pop();
+    }
+
+    public static int evaluateInfix(String expression) {
+        String postfix = infixToPostfix(expression);
+        return evaluatePostfix(postfix);
+    }
+
+    public static void main(String[] args) {
+        String exp = "3+5*(2-8)";
+        System.out.println("Infix evaluation: " + evaluateInfix(exp));
+    }
+}
